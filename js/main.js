c96039326c25acc7fe6dde84b5569aadaac3932e
bbangddeok2 <songseungil39@gmail.com>
2025-11-09 23:17:27 +0900

    모달빼고 다함

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 $(document).ready(function () {
-  var navHeight = $(".bottom_nav").outerHeight(); // 70px
+  var navHeight = $(".bottom_nav").outerHeight();
   var modalHeight = $(".reserve_modal").outerHeight();
+
+  $(".installation_wrap").each(function (index) {
+    $(this).attr("data-index", index);
+  });
+
+  $(".tab_title_3")
+    .each(function (index) {
+      $(this).attr("data-index", index);
+    })
+    .click(function () {
+      var clicked = $(this).attr("data-index");
+      $(".tab_title_3").removeClass("tab_title_3_active");
+      $(".tab_title_3").eq(clicked).addClass("tab_title_3_active");
+      $(".installation_wrap").removeClass("installation_wrap_active");
+      $(".installation_wrap").eq(clicked).addClass("installation_wrap_active");
+    });
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.3,
+    initialSlide: 1,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: true,
+    },
+    spaceBetween: 80,
+    loop: true,
+  });
 
   $(".reserve_btn").on("click", function () {
     // 모달이 네비 위에 딱 붙도록
@@ -10,5 +43,9 @@ $(document).ready(function () {
   $(".close_modal").on("click", function () {
     // 다시 숨기기
     $(".reserve_modal").css("bottom", -modalHeight + "px");
+  });
+
+  $(".heart").on("click", function () {
+    $(this).toggleClass("active");
   });
 });
